d3e74771e15d46f8aceac00d2524afb6bdbef5a8
zakzakst <e1433104@gmail.com>
2022-10-03 12:25:07 +0900

    chapter3

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+const f = (...data:number[]):number => {
+  let total = 0
+  for (let i of data) {
+    total += i
+  }
+  return total
+}
+console.log(f(1,2,3,4,5))
+console.log(f(10,20,30,40,50))
+
+
+// function printPerson(name:string = 'no-name', age:number = -1):void {
+//   console.log('Name:' + name + '(' + age + ')')
+// }
+// printPerson('Taro', 39)
+// printPerson('Hanako')
+// printPerson()
