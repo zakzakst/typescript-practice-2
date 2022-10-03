5782e45bd40d40485faeb2fbe6b7831d496f44fb
zakzakst <e1433104@gmail.com>
2022-10-03 12:50:19 +0900

    chapter3

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,35 @@ const func = (n:number, f:(n:number)=>number|string):void => {
   console.log('Result:' + res)
 }
 const double = (n:number) => n * 2
-const total = (n:number) => {
-  let total = 0
-  for (let i = 1; i <= n; i++)
-    total += i
-  return total
+const word = (n:number):string => {
+  const w = ['〇', '一', '二', '三', '四', '五', '六', '七', '八', '九']
+  const s = String(n)
+  let res:string[] = []
+  for (let i = 0; i < s.length; i++) {
+    let c = s.charAt(i)
+    res.push(w[Number(c)])
+  }
+  return res.join('')
 }
-const num = 100
+const num = 1230
 func(num, double)
-func(num, total)
+func(num, word)
+
+
+// const func = (n:number, f:(n:number)=>number|string):void => {
+//   let res = f(n)
+//   console.log('Result:' + res)
+// }
+// const double = (n:number) => n * 2
+// const total = (n:number) => {
+//   let total = 0
+//   for (let i = 1; i <= n; i++)
+//     total += i
+//   return total
+// }
+// const num = 100
+// func(num, double)
+// func(num, total)
 
 
 // const f = (n:number):void => {
